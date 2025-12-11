a0baf32a2b606e03a146e9ccbc50b866cb1b808d
leirisson <leirissonsouza99@gmail.com>
2025-12-11 13:26:51 -0400

    api 2 com python fastapi

--- 6-FAST-API/2-api-listar-produto/main.py
+++ 6-FAST-API/2-api-listar-produto/main.py
@@ -18,6 +18,13 @@ produtos = {
 def get_all_products():
     return produtos
 
+@app.get("/produtos/{id_produto}")
+async def get_product_by_id(id_produto: int):
+    produto_encontrado = produtos[id_produto]
+    print(produto_encontrado)
+    return produto_encontrado
+
+
 if __name__ == "__main__":
     import uvicorn 
     uvicorn.run("main:app", host="0.0.0.0", port=3005, debug=True, reload=True)
