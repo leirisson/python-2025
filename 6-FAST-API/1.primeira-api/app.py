9d7294a0d050f9f288b0c4ccc9e52066b365df48
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-02 04:38:42 -0400

    remove unused FastAPI example and related files

---
+++ 6-FAST-API/1.primeira-api/app.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/saudar/{nome}")
+async def saudar(nome: str):
+    print(type(nome))
+    return{
+        "mensagem": f"olá, {nome}"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=3001,
+        reload=True
+        )
